9fb1e59f614899c2d422f3b7179638705b6ad82e
JJD <vanDrunen.julia@gmail.com>
2018-01-31 09:35:27 +0100

    added ex.3

---
+++ 3.julycheck/julycheck.php
@@ -0,0 +1,9 @@
+<?php
+$month = date("m"); //displays current month as digits (01-12)
+
+if ($month == 07){
+	echo "It's July, so it can get really hot.";
+} else{
+	echo "It’s not July, so at least we’re not in the peak of the heat.";
+}
+?>
